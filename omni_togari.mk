2b9aabb249052a7c018d3505de55f78a186be647
xylon-reborn <xylon-reborn@outlook.com>
2013-11-29 10:53:15 +0100

    Omnify

---
+++ omni_togari.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/sony/togari/full_togari.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903_1276-7948/C6903:4.2.2/14.1.G.1.526/8Xl-jw:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.2.2 14.1.G.1.526 8Xl-jw test-keys"
+
+PRODUCT_NAME := omni_togari
+PRODUCT_DEVICE := togari
