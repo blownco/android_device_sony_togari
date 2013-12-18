994a25edc0501a77878e6f11a731f16e22769ecb
xylon-reborn <xylon-reborn@outlook.com>
2013-12-18 15:00:12 +0100

    Update build fingerprint to 4.4.2 GPE one

--- omni_togari.mk
+++ omni_togari.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903_1276-7948/C6903:4.2.2/14.1.G.1.526/8Xl-jw:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.2.2 14.1.G.1.526 8Xl-jw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6806_GPe/C6806:4.4.2/KOT49H.S2/2052:user/release-keys PRIVATE_BUILD_DESC="C6806-user 4.4.2 KOT49H.S2 2052 test-keys"
 
 PRODUCT_NAME := omni_togari
 PRODUCT_DEVICE := togari
